0f03f2c3b2798667c55492506430cfff4081d7f2
Bruno Nascimento Ferreira <bruno.nasc@msn.com>
2015-11-09 22:09:02 -0200

    adicionando saida no eexcel

--- Metrics/MetricPrinter/ExcelPrinter.cs
+++ Metrics/MetricPrinter/ExcelPrinter.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
 using System.Data.OleDb;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Metrics.MetricPrinter
 {
